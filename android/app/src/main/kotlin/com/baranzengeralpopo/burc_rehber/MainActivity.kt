40a1cc4af282f3b4b50bb68c153a4b8d2c649460
Baran Zengeralp <73583683+baran2101@users.noreply.github.com>
2022-04-29 03:10:16 +0300

    Delete android directory

--- android/app/src/main/kotlin/com/baranzengeralpopo/burc_rehber/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.baranzengeralpopo.burc_rehber
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/baranzzengeralpp/burc_rehber/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.baranzzengeralpp.burc_rehber
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
